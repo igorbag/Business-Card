908014841c50c87d73eb5da81ba9f757c7a357ca
igor.bagliotti <igorbag@hotmail.com>
2021-05-29 17:03:31 -0300

    Adicionar metodo salvar

--- app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import br.com.dio.businesscard.App
-import br.com.dio.businesscard.R
+import br.com.dio.businesscard.data.BusinessCard
+import br.com.dio.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
 
     private val mainViewModel: MainViewModel by viewModels {
         MainViewModelFactory((application as App).repository)
@@ -14,12 +17,23 @@ class AddBusinessCardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_business_card)
+        setContentView(binding.root)
         insertListeners()
     }
 
     private fun insertListeners() {
-
+        binding.btnConfirm.setOnClickListener {
+            val businessCard = BusinessCard(
+                id = 1,
+                nome = binding.tilNome?.editText?.text.toString(),
+                empresa = binding.tilEmpresa?.editText?.text.toString(),
+                telefone = binding.tilTelefone?.editText?.text.toString(),
+                email = binding.tilEmail?.editText?.text.toString(),
+                fundoPersonalizado = binding.tilCor?.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+        }
     }
+
 
 }
